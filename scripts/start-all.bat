dabba110f0821bc58bc214d70279805fe3d1f4c5
Dasha <m4daria@gmail.com>
2025-04-29 22:49:12 +0100

    start all upd FINAL

--- scripts/start-all.bat
+++ scripts/start-all.bat
@@ -9,7 +9,7 @@ for %%N in (A B C) do (
     echo ——— Запускаем узел %%N ———
     for %%I in (1 2 3 4) do (
         echo   • сервер %%N-%%I…
-        forever start C:\ds\server.js configs\node%%N\server%%I.json
+        forever start server.js configs\node%%N\server%%I.json
         timeout /t 1 >nul
     )
 )
@@ -17,4 +17,3 @@ for %%N in (A B C) do (
 echo.
 echo ✅ Все 12 серверов запущены!
 pause
-
